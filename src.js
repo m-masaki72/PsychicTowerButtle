67aa5f316cccba3ef080da3bbd78fa56a67990f7
masaki mori <mementomori7272@gmail.com>
2018-03-13 03:25:22 +0900

    Update src.js

--- src.js
+++ src.js
@@ -1,56 +1,41 @@
-// canvas上のイメージを保存
-function saveCanvas(saveType){
-    var imageType = "image/png";
-    var fileName = "sample.png";
-    if(saveType === "jpeg"){
-        imageType = "image/jpeg";
-        fileName = "sample.jpg";
-    }
-    var canvas = document.getElementById("gameContainer");
-    // base64エンコードされたデータを取得 「data:image/png;base64,iVBORw0k～」
-    var base64 = canvas.toDataURL(imageType);
-    // base64データをblobに変換
-    var blob = Base64toBlob(base64);
-    // blobデータをa要素を使ってダウンロード
-    saveBlob(blob, fileName);
+var container;
+var canvas;
+var timer = 0;
+
+//初期化
+function init() {
+canvas = document.getElementById('canvas');
+container = document.createElement('div');
+container.style.width = window.innerWidth + 'px';
+container.style.height = window.innerHeight + 'px';
+container.style.overflow = 'hidden';
+container.appendChild(canvas);
+document.body.appendChild(container);
+document.body.style.margin = '0px'
 }
 
-// Base64データをBlobデータに変換
-function Base64toBlob(base64)
-{
-    // カンマで分割して以下のようにデータを分ける
-    // tmp[0] : データ形式（data:image/png;base64）
-    // tmp[1] : base64データ（iVBORw0k～）
-    var tmp = base64.split(',');
-    // base64データの文字列をデコード
-    var data = atob(tmp[1]);
-    // tmp[0]の文字列（data:image/png;base64）からコンテンツタイプ（image/png）部分を取得
-	var mime = tmp[0].split(':')[1].split(';')[0];
-    //  1文字ごとにUTF-16コードを表す 0から65535 の整数を取得
-	var buf = new Uint8Array(data.length);
-	for (var i = 0; i < data.length; i++) {
-        buf[i] = data.charCodeAt(i);
-    }
-    // blobデータを作成
-	var blob = new Blob([buf], { type: mime });
-    return blob;
+//サイズ変更処理
+function resize() {
+container.style.width = window.innerWidth + 'px';
+container.style.height = window.innerHeight + 'px';
+canvas.width = window.innerWidth * window.devicePixelRatio;
+canvas.height = window.innerHeight * window.devicePixelRatio;
 }
 
-// 画像のダウンロード
-function saveBlob(blob, fileName)
-{
-    var url = (window.URL || window.webkitURL);
-    // ダウンロード用のURL作成
-    var dataUrl = url.createObjectURL(blob);
-    // イベント作成
-    var event = document.createEvent("MouseEvents");
-    event.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    // a要素を作成
-    var a = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
-    // ダウンロード用のURLセット
-    a.href = dataUrl;
-    // ファイル名セット
-    a.download = fileName;
-    // イベントの発火
-    a.dispatchEvent(event);
+window.onload = function () {
+init();
+resize();
+};
+
+//ブラウザの大きさが変わった時に行う処理
+(function () {
+var timer = 0;
+window.onresize = function () {
+if (timer > 0) {
+clearTimeout(timer);
 }
+
+    timer = setTimeout(function () {
+        resize();
+    }, 200);
+};
